d88d5220c9988c0f620165b700b47f0018afea92
SKumarMetro <s.kumar@metrological.com>
2018-10-09 15:44:11 -0700

    Sample in-process plugin

--- Sample.cpp
+++ Sample.cpp
@@ -10,83 +10,45 @@ namespace Plugin {
 
     /* virtual */ const string Sample::Initialize(PluginHost::IShell* service)
     {
+        ASSERT (_service == nullptr);
+        ASSERT (service != nullptr);
+
         Config config;
-        string message;
-
-        ASSERT(_service == nullptr);
-        ASSERT(_implementation == nullptr);
-
-        _pid = 0;
+        config.FromString(service->ConfigLine());
+        _skipURL = static_cast<uint8_t>(service->WebPrefix().length());
         _service = service;
-        _skipURL = static_cast<uint8_t>(_service->WebPrefix().length());
-        config.FromString(_service->ConfigLine());
 
-        // Register the Process::Notification stuff. The Remote process might die before we get a
-        // change to "register" the sink for these events !!! So do it ahead of instantiation.
-        _service->Register(&_notification);
+        AsyncStatus("UP");
 
-        _implementation = _service->Root<Exchange::ISample>(_pid, 2000, _T("SampleImplementation"));
-
-        if (_implementation == nullptr) {
-            message = _T("Sample could not be instantiated.");
-            _service->Unregister(&_notification);
-            _service = nullptr;
-        }
-        else {
-            _implementation->Configure(_service);
-       }
-
-        return message;
+        return (string());
     }
 
-    /*virtual*/ void Sample::Deinitialize(PluginHost::IShell* service)
+    /* virtual */ void Sample::Deinitialize(PluginHost::IShell* service)
     {
-        ASSERT(_service == service);
-        ASSERT(_implementation != nullptr);
+        ASSERT (_service == service);
+        AsyncStatus("DOWN");
 
-        _service->Unregister(&_notification);
-
-        if (_implementation->Release() != Core::ERROR_DESTRUCTION_SUCCEEDED) {
-
-            ASSERT(_pid != 0);
-
-            TRACE_L1("Sample Plugin is not properly destructed. %d", _pid);
-
-            RPC::IRemoteProcess* process(_service->RemoteProcess(_pid));
-
-            // The process can disappear in the meantime...
-            if (process != nullptr) {
-
-                // But if it did not dissapear in the meantime, forcefully terminate it. Shoot to kill :-)
-                process->Terminate();
-                process->Release();
-            }
-        }
-
-        // Deinitialize what we initialized..
-        _implementation = nullptr;
         _service = nullptr;
     }
 
     /* virtual */ string Sample::Information() const
     {
-        // No additional info to report.
-        return (nullptr);
+        return (string());          // No additional info to report.
     }
 
-    /* virtual */ void Sample::Inbound(WPEFramework::Web::Request& request)
+    /* virtual */ void Sample::Inbound(Web::Request& request )
     {
-    if (request.Verb == Web::Request::HTTP_POST)
-        request.Body(jsonBodyDataFactory.Element());
+        if (request.Verb == Web::Request::HTTP_POST)
+            request.Body(jsonBodyDataFactory.Element());
     }
 
     /*------------------------------------------------------------------------
-     *  $ curl -X GET  http://bcm/Service/Sample/Test
+     *  $ curl -X GET  http://<HOST>/Service/Sample/Test
      *  {"Str":"Nothing set"}
      *
      *  $ curl -X POST -d '{"Str":"TEST"}' http://bcm/Service/Sample/Test
      *
-     *  $ curl -X GET  http://bcm/Service/Sample/Test
+     *  $ curl -X GET  http://<HOST>/Service/Sample/Test
      *  {"Str":"TEST"}
      *------------------------------------------------------------------------*/
     /* virtual */ Core::ProxyType<Web::Response> Sample::Process(const WPEFramework::Web::Request& request)
@@ -104,7 +66,7 @@ namespace Plugin {
 
             if (index.Current().Text() == _T("Test")) {
                 Core::ProxyType<Web::JSONBodyType<Data> > data (jsonDataFactory.Element());
-                data->Str = _implementation->SampleGet();
+                data->Str = _testStr;
                 result->ContentType = Web::MIMETypes::MIME_JSON;
                 result->Body(data);
             } else {
@@ -114,10 +76,7 @@ namespace Plugin {
         } else if ((request.Verb == Web::Request::HTTP_POST) && ((index.Next()) && (index.Next()))) {
 
             if (index.Current().Text() == _T("Test")) {
-                std::string str = request.Body<const Data>()->Str.Value();
-
-                _implementation->SampleSet(str);
-
+                _testStr = request.Body<const Data>()->Str.Value();
                 result->ErrorCode = Web::STATUS_OK;
                 result->Message = "OK";
             } else {
@@ -131,16 +90,5 @@ namespace Plugin {
         return result;
     }
 
-    void Sample::Deactivated(RPC::IRemoteProcess* process)
-    {
-        if (process->Id() == _pid) {
-
-            ASSERT(_service != nullptr);
-
-            PluginHost::WorkerPool::Instance().Submit(PluginHost::IShell::Job::Create(_service,
-                PluginHost::IShell::DEACTIVATED,
-                PluginHost::IShell::FAILURE));
-        }
-    }
-}
-} //namespace WPEFramework::Plugin
+} // namespace Plugin
+} // namespace WPEFramework
--- Sample.h
+++ Sample.h
@@ -1,8 +1,7 @@
-#ifndef __SAMPLE_H
-#define __SAMPLE_H
+#ifndef SAMPLE_H
+#define SAMPLE_H
 
 #include "Module.h"
-#include <interfaces/ISample.h>
 
 namespace WPEFramework {
 namespace Plugin {
@@ -11,58 +10,6 @@ namespace Plugin {
     private:
         Sample(const Sample&) = delete;
         Sample& operator=(const Sample&) = delete;
-
-        class Notification : public RPC::IRemoteProcess::INotification {
-
-        private:
-            Notification() = delete;
-            Notification(const Notification&) = delete;
-            Notification& operator=(const Notification&) = delete;
-
-        public:
-            explicit Notification(Sample* parent)
-                : _parent(*parent)
-            {
-                ASSERT(parent != nullptr);
-            }
-            ~Notification()
-            {
-            }
-
-        public:
-            virtual void Activated(RPC::IRemoteProcess*) {
-            }
-            virtual void Deactivated(RPC::IRemoteProcess* process) {
-                _parent.Deactivated(process);
-            }
-
-            BEGIN_INTERFACE_MAP(Notification)
-                INTERFACE_ENTRY(RPC::IRemoteProcess::INotification)
-            END_INTERFACE_MAP
-
-        private:
-            Sample& _parent;
-        };
-
-        class Config : public Core::JSON::Container {
-        private:
-            Config(const Config&) = delete;
-            Config& operator=(const Config&) = delete;
-
-        public:
-            Config()
-                : Core::JSON::Container()
-                , OutOfProcess(true)
-            {
-                Add(_T("outofprocess"), &OutOfProcess);
-            }
-            ~Config()
-            {
-            }
-
-        public:
-            Core::JSON::Boolean OutOfProcess;
-        };
 
     public:
         class Data : public Core::JSON::Container {
@@ -85,12 +32,11 @@ namespace Plugin {
             Core::JSON::String Str;
         };
 
-
     public:
         Sample()
-            : _service(nullptr)
-            , _implementation(nullptr)
-            , _notification(this)
+            : _skipURL(0)
+            , _service(nullptr)
+            , _testStr("Nothing Set")
         {
         }
 
@@ -98,52 +44,37 @@ namespace Plugin {
         {
         }
 
-    public:
         BEGIN_INTERFACE_MAP(Sample)
         INTERFACE_ENTRY(PluginHost::IPlugin)
         INTERFACE_ENTRY(PluginHost::IWeb)
-        INTERFACE_AGGREGATE(Exchange::ISample, _implementation)
         END_INTERFACE_MAP
 
     public:
-        //  IPlugin methods
+        //   IPlugin methods
         // -------------------------------------------------------------------------------------------------------
+        virtual const string Initialize(PluginHost::IShell* service) override;
+        virtual void Deinitialize(PluginHost::IShell* service) override;
+        virtual string Information() const override;
 
-        // First time initialization. Whenever a plugin is loaded, it is offered a Service object with relevant
-        // information and services for this particular plugin. The Service object contains configuration information that
-        // can be used to initialize the plugin correctly. If Initialization succeeds, return nothing (empty string)
-        // If there is an error, return a string describing the issue why the initialisation failed.
-        // The Service object is *NOT* reference counted, lifetime ends if the plugin is deactivated.
-        // The lifetime of the Service object is guaranteed till the deinitialize method is called.
-        virtual const string Initialize(PluginHost::IShell* service);
-
-        // The plugin is unloaded from the webbridge. This is call allows the module to notify clients
-        // or to persist information if needed. After this call the plugin will unlink from the service path
-        // and be deactivated. The Service object is the same as passed in during the Initialize.
-        // After theis call, the lifetime of the Service object ends.
-        virtual void Deinitialize(PluginHost::IShell* service);
-
-        // Returns an interface to a JSON struct that can be used to return specific metadata information with respect
-        // to this plugin. This Metadata can be used by the MetData plugin to publish this information to the ouside world.
-        virtual string Information() const;
-
-        //  IWeb methods
+        //   IWeb methods
         // -------------------------------------------------------------------------------------------------------
-        virtual void Inbound(Web::Request& request);
-        virtual Core::ProxyType<Web::Response> Process(const Web::Request& request);
+        virtual void Inbound(Web::Request& request) override;
+        virtual Core::ProxyType<Web::Response> Process(const Web::Request& request) override;
 
     private:
-        void Deactivated(RPC::IRemoteProcess* process);
+        void AsyncStatus(const string& status) {
+            TRACE_L1("Sending async status %s", status.c_str());
+            string message(string("{ \"Status\": \"" + status + "\" }"));
+            _service->Notify(message);  // Send a message to the websocket
+        }
 
     private:
         uint8_t _skipURL;
-        uint32_t _pid;
         PluginHost::IShell* _service;
-        Exchange::ISample* _implementation;
-        Core::Sink<Notification> _notification;
+        std::string _testStr;
     };
-} //namespace Plugin
 
-} //namespace Solution
+} // namespace Plugin
+} // namespace WPEFramework
 
-#endif // __SAMPLE_H
+#endif // SAMPLE_H
